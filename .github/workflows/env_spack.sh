4e027189e8ddf09f5524fdf31f0bae22feddc37f
franck.ledoux <franck.ledoux@cea.fr>
2022-12-20 10:24:40 +0100

    - new workflow for spack

---
+++ .github/workflows/env_spack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone --depth=1 -b releases/latest https://github.com/spack/spack.git
+sed -i 's#"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"#"${PACKAGE}"#g' spack/etc/spack/defaults/config.yaml
+. ./spack/share/spack/setup-env.sh
+
+git clone --depth=1 https://github.com/LIHPC-Computational-Geometry/spack_recipes_meshing.git
+
+spack repo add ./spack_recipes_meshing/meshing_repo
+spack repo add ./spack_recipes_meshing/supersede_repo
+
+spack external find cmake
+spack install lcov
+spack install swig
